47f1da255dffa7f4451b2cb835293db5155a436f
MlrariA <kuzmin1914@gmail.com>
2023-12-17 23:26:46 +0700

    kakoi-to login fourth

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,13 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends AbstractController
 {
+//    public function __construct(
+//        private EntityManagerInterface $entityManager
+//    )
+//    {
+//
+//    }
+
     public function index(): Response
     {
+//        $user = new User();
+//        $user->setEmail('kuzmin1914@gmail.com')
+//             ->setPassword('$2y$13$/9S0Ac8JK0SsVbKt/as/vuBa/.cDguSaZ6BY9LOmu5qWRGOrMdDim');
+//        $this->entityManager->persist($user);
+//        $this->entityManager->flush();
         return $this->render('blog.html.twig');
     }
 }

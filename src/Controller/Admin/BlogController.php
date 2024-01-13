feaa2ce1bf474e509d3e021119eb2efcf3e99cc9
MlrariA <kuzmin1914@gmail.com>
2024-01-14 00:18:23 +0700

    tyajelo blog sixth

--- src/Controller/Admin/BlogController.php
+++ src/Controller/Admin/BlogController.php
@@ -2,7 +2,52 @@
 
 namespace App\Controller\Admin;
 
-class BlogController
-{
+use App\Entity\Post;
+use App\Form\PostType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
+class BlogController extends AbstractController
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager
+    )
+    {
+    }
+
+    public function createPost(Request $request): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post->setUser($this->getUser());
+            $this->entityManager->persist($post);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('blog/blogForm.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    public function editPost(Post $post, Request $request): Response
+    {
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($post);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('blog/blogForm.html.twig', [
+            'form' => $form,
+        ]);
+    }
 }
---
+++ src/Form/PostType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('content', TextareaType::class, [
+                'attr' => ['max' => 3000],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Напишите новый пост',
+                    ]),
+                    new Length([
+                        'max' => 3000,
+                        'maxMessage' => 'Вы превысили допустимое количество символов',
+                    ]),
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}

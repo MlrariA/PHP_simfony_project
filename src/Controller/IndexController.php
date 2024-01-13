a884f41e9b025d5baf92bf5dcd4d6dfaf4d876f3
MlrariA <kuzmin1914@gmail.com>
2024-01-13 21:30:45 +0700

    nu uje nachalo blog fifth

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\PostRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,20 +11,17 @@ use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends AbstractController
 {
-//    public function __construct(
-//        private EntityManagerInterface $entityManager
-//    )
-//    {
-//
-//    }
+    public function __construct(
+        private PostRepository $postRepository
+    )
+    {
+
+    }
 
     public function index(): Response
     {
-//        $user = new User();
-//        $user->setEmail('kuzmin1914@gmail.com')
-//             ->setPassword('$2y$13$/9S0Ac8JK0SsVbKt/as/vuBa/.cDguSaZ6BY9LOmu5qWRGOrMdDim');
-//        $this->entityManager->persist($user);
-//        $this->entityManager->flush();
-        return $this->render('blog.html.twig');
+        return $this->render('blog/blog.html.twig',[
+            'posts'=> $this->postRepository->findAll()
+        ]);
     }
 }
--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,12 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher,
+        private readonly EntityManagerInterface      $entityManager
+    ) {}
+
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // get the login error if there is one
@@ -21,6 +31,30 @@ class LoginController extends AbstractController
         ]);
     }
 
+    public function registration(Request $request): Response
+    {
+
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $hashedPassword = $this->passwordHasher->hashPassword(
+                $user,
+                $form->get('plainPassword')->getData()
+            );
+
+            $user->setPassword($hashedPassword);
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'form' => $form,
+        ]);
+    }
     public function logout()
     {
         

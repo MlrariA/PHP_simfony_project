db54f5d0e55d78389ccf942612864e39a84ea307
MlrariA <kuzmin1914@gmail.com>
2024-01-14 21:03:45 +0700

    xdd unit test ninth

---
+++ tests/UnitTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class UnitTest extends TestCase
+{
+
+    public function setUp(): void
+    {
+
+    }
+
+    public function testCommentApprove(): void
+    {
+        $this->assertEquals(4, (2 + 2));
+    }
+}
